448fb30899086f3942d57b2ae0e8f65b8a046c4e
Quinntyne Brown <quinntynebrown@gmail.com>
2015-12-27 14:43:50 -0500

    feat(home): rotator

--- src/app/components/wb-home.js
+++ src/app/components/wb-home.js
@@ -5,7 +5,11 @@
     function HomeComponent() {
         var self = this;
 
-
+        self.photos = [
+            { imageUrl: "/assets/images/carousel_1.jpg", name: "carousel_1.jpg", title: "bride" },
+            { imageUrl: "/assets/images/carousel_2.jpg", name: "carousel_2.jpg", title: "cake" },
+            { imageUrl: "/assets/images/carousel_3.jpg", name: "carousel_3.jpg", title: "dress" }
+        ];
 
         return self;
     }
