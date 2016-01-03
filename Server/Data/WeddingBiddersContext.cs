472681f5de1b9c1bd6e71f8dca86ae2f8579d40f
Quinntyne Brown <quinntynebrown@gmail.com>
2016-01-02 19:26:04 -0500

    feat(): ef model builder

--- Server/Data/WeddingBiddersContext.cs
+++ Server/Data/WeddingBiddersContext.cs
@@ -47,5 +47,56 @@ namespace WeddingBidders.Server.Data
         public DbSet<HtmlContent> HtmlContents { get; set; }
 
         public DbSet<Conversation> Conversations { get; set; }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in this.ChangeTracker.Entries()
+            .Where(e => e.Entity is ILoggable &&
+                ((e.State == EntityState.Added || (e.State == EntityState.Modified)))))
+            {
+
+                if (((ILoggable)entry.Entity).CreatedDate == null)
+                {
+                    ((ILoggable)entry.Entity).CreatedDate = DateTime.UtcNow;
+                }
+
+                ((ILoggable)entry.Entity).LastModifiedDate = DateTime.UtcNow;
+
+            }
+
+            return base.SaveChanges();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().
+
+                HasMany(u => u.Roles).
+                WithMany(r => r.Users).
+
+                Map(
+                    m =>
+                    {
+                        m.MapLeftKey("User_Id");
+                        m.MapRightKey("Role_Id");
+                        m.ToTable("UserRoles");
+                    });
+
+            modelBuilder.Entity<User>().
+
+                HasMany(u => u.Groups).
+                WithMany(g => g.Users).
+
+                Map(
+                    m =>
+                    {
+                        m.MapLeftKey("User_Id");
+                        m.MapRightKey("Group_Id");
+                        m.ToTable("UserGroups");
+                    });
+        }
+
+
     }
+    
 }
